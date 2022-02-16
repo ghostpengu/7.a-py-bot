478dc95ecbad042c046cf57cced515b1eef0f163
Pingu <77410713+ghostpengu@users.noreply.github.com>
2022-02-16 17:38:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import ctypes
 from http import client
 import os
 import string
+import random
 
 import discord
 from discord.ext import commands
@@ -15,7 +17,28 @@ async def pleska(ctx):
     
 
 @bot.command()
+async def ask(ctx,*,Question):
+    responses = ["Ano",
+                "Nie",
+                "Mozno",
+                "Urcite",
+                "Si Gay",
+                "Ok",
+                "Idk",
+                "Neviem",
+                "alex",
+                "Ano a mas maly pp btw",
+                "ti jebe",
+                "NN",
+                "Jj",
+                "Ti Mrda?"]
+ 
+    await ctx.send(f'Otazka: {Question} Odpoved: {random.choice(responses)}')
+
+@bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    embed=discord.Embed()
+    embed.add_field(name="Ping", value="Pong", inline=False)
+    await ctx.send(embed=embed)
     
 bot.run(tokenforbot)
